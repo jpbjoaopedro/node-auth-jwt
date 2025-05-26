9c33267f57bf439b7b4ed495b11a521bb5fce275
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2025-05-26 13:41:57 -0300

    Add Swagger UI integration and update dependencies in package.json

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors, { CorsOptions } from 'cors';
 import { router } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 export function createApp () {
     const app = express();
@@ -13,6 +15,11 @@ export function createApp () {
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use('/api', router)
+
+    // Carrega documentação Swagger
+    const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     return app;
 };
